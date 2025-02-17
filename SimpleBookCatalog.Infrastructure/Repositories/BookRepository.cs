ddc9961f3a52cc55c5bcce90fd297925cf79ccf1
Sweta Panchal <sweta.panchal@nexuslinkservices.in>
2025-02-17 19:25:25 +0530

    Add project files.

---
+++ SimpleBookCatalog.Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using SimpleBookCatalog.Application.Interfaces;
+using SimpleBookCatalog.Domain.Entities;
+using SimpleBookCatalog.Infrastructure.Context;
+
+namespace SimpleBookCatalog.Infrastructure.Repositories
+{
+    public class BookRepository :IBookRepository
+    {
+        private readonly SimpleBookCatalogDbContext context;
+        public BookRepository(IDbContextFactory<SimpleBookCatalogDbContext> factory)
+        {
+            context = factory.CreateDbContext();
+        }
+
+		public async Task AddAsync(Book book)
+		{
+            context.Books.Add(book);
+            await context.SaveChangesAsync();
+		}
+	}
+}
